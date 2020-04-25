ca6c47a8b446ea8a9687fe36080bb5e31cdba034
mtoRex <64027180+mtoRex@users.noreply.github.com>
2020-04-25 22:57:11 +0600

    cachematrix solution is added

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Update two function makeCacheMatrix & cacheSolve with proper evalution
+## First one is makeCacheMatrix where some variable and function intialized 
+## Second one cacheSolve get putMark funcation called and returned inverse matrix.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mat = matrix()) {
+  m_val <- NULL
+  set <- function(y_val) {
+    mat <<- y_val
+    m_val <<- NULL
+  }
+  get <- function() mat
+  setResult <- function(solve) m_val <<- solve
+  getResult <- function() m_val
+        
+  # list constructor
+  list(set = set, get = get,
+       setResult = setResult,
+       getResult = getResult)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(mat, ...) {
+  m_val <- mat$getResult() ## put the function value in m_val 
+  if(!is.null(m_val)) {
+    message("getting cached data")
+    return(m_val) ## Return the matrix that is the inverse of 'mat'
+  }
+  data <- mat$get()
+  m_val <- solve(data, ...)
+  mat$setSolve(m_val)
+  m_val
 }
